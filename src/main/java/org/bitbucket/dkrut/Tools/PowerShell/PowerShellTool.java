bf60a677a5ed3afa8249cffc5fb009eff0a9b27d
master <dd.krutikov@gmail.com>
2019-01-08 13:50:54 +0300

    powershellTool created

---
+++ src/main/java/org/bitbucket/dkrut/Tools/PowerShell/PowerShellTool.java
@@ -0,0 +1,26 @@
+package org.bitbucket.dkrut.Tools.PowerShell;
+
+import com.profesorfalken.jpowershell.PowerShell;
+
+/**
+ * Created by Denis Krutikov on 08.01.2019.
+ */
+
+public class PowerShellTool {
+    private PowerShell powerShell;
+
+    private void createSession(){
+        powerShell = PowerShell.openSession();
+    }
+
+    private void closeSession(){
+        powerShell.close();
+    }
+
+    public String getCommandOutput(String scriptPath){
+        createSession();
+        String commandOutput = powerShell.executeScript(scriptPath).getCommandOutput();
+        closeSession();
+        return commandOutput;
+    }
+}

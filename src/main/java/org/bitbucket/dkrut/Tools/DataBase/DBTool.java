cf500195189ddb909663c5fe878889c902ca6914
master <dd.krutikov@gmail.com>
2019-01-08 15:11:44 +0300

    DBTool created

---
+++ src/main/java/org/bitbucket/dkrut/Tools/DataBase/DBTool.java
@@ -0,0 +1,50 @@
+package org.bitbucket.dkrut.Tools.DataBase;
+
+import java.sql.*;
+
+/**
+ * Created by Denis Krutikov on 08.01.2019.
+ */
+
+public class DBTool {
+
+    public void mssql(String query){
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://SERVER:PORT;DatabaseName=DB_NAME";
+            String login = "LOGIN";
+            String password = "PASSWORD";
+
+            try (Connection connection = DriverManager.getConnection(url, login, password)) {
+                Statement statement = connection.createStatement();
+                statement.executeUpdate(query);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace(); // обработка ошибки  Class.forName
+            System.out.println("JDBC драйвер для СУБД не найден!");
+        } catch (SQLException e) {
+            e.printStackTrace(); // обработка ошибок  DriverManager.getConnection
+            System.out.println("Ошибка SQL !");
+        }
+    }
+
+    public void pg(String query){
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://SERVER:PORT/DB_NAME";
+            String login = "LOGIN";
+            String password = "PASSWORD";
+
+            try (Connection connection = DriverManager.getConnection(url, login, password)) {
+                Statement statement = connection.createStatement();
+                statement.executeUpdate(query);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace(); // обработка ошибки  Class.forName
+            System.out.println("JDBC драйвер для СУБД не найден!");
+        } catch (SQLException e) {
+            e.printStackTrace(); // обработка ошибок  DriverManager.getConnection
+            System.out.println("Ошибка SQL !");
+        }
+    }
+}

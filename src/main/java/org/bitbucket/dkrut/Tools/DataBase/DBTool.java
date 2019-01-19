f22e87ce3fa53c155c304f53fbb532484338cda9
master <dd.krutikov@gmail.com>
2019-01-19 20:18:28 +0300

    method for get column value (DBTool)

--- src/main/java/org/bitbucket/dkrut/Tools/DataBase/DBTool.java
+++ src/main/java/org/bitbucket/dkrut/Tools/DataBase/DBTool.java
@@ -47,4 +47,56 @@ public class DBTool {
             System.out.println("Ошибка SQL !");
         }
     }
+
+    public String getValueMssql (String query, String columnName){
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String url = "jdbc:sqlserver://SERVER:PORT;DatabaseName=DB_NAME";
+            String login = "LOGIN";
+            String password = "PASSWORD";
+
+            try (Connection connection = DriverManager.getConnection(url, login, password)) {
+                Statement statement = connection.createStatement();
+                ResultSet result = statement.executeQuery(query);
+
+                while (result.next()) {
+                    return result.getString(columnName);
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace(); // обработка ошибки  Class.forName
+            System.out.println("JDBC драйвер для СУБД не найден!");
+        } catch (SQLException e) {
+            e.printStackTrace(); // обработка ошибок  DriverManager.getConnection
+            System.out.println("Ошибка SQL !");
+        }
+
+        return null;
+    }
+
+    public String getValuePg (String query, String columnName){
+        try {
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://SERVER:PORT/DB_NAME";
+            String login = "LOGIN";
+            String password = "PASSWORD";
+
+            try (Connection connection = DriverManager.getConnection(url, login, password)) {
+                Statement statement = connection.createStatement();
+                ResultSet result = statement.executeQuery(query);
+
+                while (result.next()) {
+                    return result.getString(columnName);
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace(); // обработка ошибки  Class.forName
+            System.out.println("JDBC драйвер для СУБД не найден!");
+        } catch (SQLException e) {
+            e.printStackTrace(); // обработка ошибок  DriverManager.getConnection
+            System.out.println("Ошибка SQL !");
+        }
+
+        return null;
+    }
 }

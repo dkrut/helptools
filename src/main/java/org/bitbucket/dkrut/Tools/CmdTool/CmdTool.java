b6ba83455011618e2b01792dd9ecf95ab4ece9ad
master <dd.krutikov@gmail.com>
2019-01-21 22:42:40 +0300

    CmdTool created

---
+++ src/main/java/org/bitbucket/dkrut/Tools/CmdTool/CmdTool.java
@@ -0,0 +1,41 @@
+package org.bitbucket.dkrut.Tools.CmdTool;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by Denis Krutikov on 21.01.2019.
+ */
+
+public class CmdTool {
+
+    public void executeCommand(String command) {
+        try {
+            Runtime rt = Runtime.getRuntime();
+            Process p = rt.exec("cmd /c " + command);
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(p.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getCommandOutput(String command) {
+        String result;
+        try {
+            Runtime rt = Runtime.getRuntime();
+            Process p = rt.exec("cmd /c " + command);
+            BufferedReader stdInput = new BufferedReader(new
+                    InputStreamReader(p.getInputStream()));
+
+            while ((result = stdInput.readLine()) != null) {
+            return result;
+            }
+            System.exit(0);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
